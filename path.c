69dc2100667af93b506c7ad97942d029ac47b44c
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-04-15 01:54:15 +0000

    Path test

--- path.c
+++ path.c
@@ -1,9 +1,17 @@
 #include "holberton.h"
 
 /**
+ *find_path - find PATH
  *
+ *@environ: environ variable
  *
- *
+ *Return: string with PATH, Null if it fails
  */
 
-
+char *find_path(char **environ)
+{
+	if (environ == NULL)
+		return (NULL);
+	
+	return (NULL);
+}
